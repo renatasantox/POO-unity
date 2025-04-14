869998e5b98a94430eaeb924c30a60a301dc1e1b
renatasantox <renatarafaelapereiradejesis@gmail.com>
2025-04-14 10:21:04 -0300

    Caminho do projeto

--- POO - UNITY/Assets/Carro.cs
+++ POO - UNITY/Assets/Carro.cs
@@ -2,27 +2,57 @@ using UnityEngine;
 
 public class Carro
 {
-  private string nome;
-  private int numero_de_rodas;
-  private string cor;
-  private float preco;
+    private string nome;
+    private int numero_de_rodas;
+    private string cor;
+    private float preco;
 
-  public string getNome()
-  {
-      return nome;
-  }
+    public string getNome()
+    {
+        return nome;
+    }
 
-  public void setNome(string nome)
-  {
-      this.nome = nome;
-  }
+    public void setNome(string nome)
+    {
+        this.nome = nome;
+    }
 
-  public string getCor()
-  {
-      return this.cor;
-  }
-  public void setCor(string cor)
-  {
-      this.cor = cor;
-  }
-} 
+    public string getCor()
+    {
+        return this.cor;
+    }
+
+    public void setCor(string cor)
+    {
+        this.cor = cor;
+    }
+
+    public int getNumero_de_rodas()
+    {
+        return this.numero_de_rodas;
+    }
+
+    public void setNumero_de_rodas(int numero_de_rodas)
+    {
+        this.numero_de_rodas = numero_de_rodas;
+    }
+
+    public float getPreco()
+    {
+        return this.preco;
+    }
+
+    public void setPreco(float preco)
+    {
+        this.preco = preco;
+    }
+
+    public bool ComparePreco(Carro Carro)
+    {
+        if (this.preco > Carro.getPreco())
+        {
+            return true;
+        }
+        return false;
+    }
+}
--- POO - UNITY/Assets/Teste.cs
+++ POO - UNITY/Assets/Teste.cs
@@ -3,14 +3,32 @@ using UnityEngine;
 public class Teste : MonoBehaviour
 {
     Carro fusca = new Carro();
-    private Carro gol = new Carro();
-    
-    
+    Carro gol = new Carro();
+
+
     void Start()
     {
         fusca.setNome("Fusca de pai");
         fusca.setCor("Azul");
+        fusca.setNumero_de_rodas(4);
+        fusca.setPreco(2050.90f);
+        Debug.Log("0" + fusca.getNome() + " é " + fusca.getCor() + "tem" + fusca.getNumero_de_rodas() + "e custa R$" +
+                  fusca.getPreco());
 
-        Debug.Log("0" + fusca.getNome() + " é " + fusca.getCor());
+
+        gol.setNome("Gol de Pópó");
+        gol.setCor("Azul");
+        gol.setNumero_de_rodas(4);
+        gol.setPreco(2999.99f);
+        Debug.Log("0" + gol.getNome());
+
+        if (fusca.ComparePreco(gol))
+        {
+            Debug.Log("O carro" + fusca.getNome() + "é mais caro do que o carro" + gol.getNome());
+        }
+        else
+        {
+            Debug.Log("O carro" + fusca.getNome() + "é mais barato do que o carro" + gol.getNome());
+        }
     }
 }
